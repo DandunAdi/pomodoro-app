a9d3d2250a475ec22ce4cb4cc3e0ba07a0c72b64
Dandun Adi <dandun.adi@gmail.com>
2021-06-07 04:47:39 +0700

    add timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 const Timer = () => {
+  const [time, setTime] = useState("25:00");
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
+
+  const handleTimer = () => {
+    if (isTimerRunning) return;
+
+    setIsTimerRunning(!isTimerRunning);
+    startTimer(25 * 60, setTime, () => setIsTimerRunning(false));
+  };
+
   return (
     <div className="Timer text-center flex flex-column">
       <div className="my-2 flex shadow" style={timeContainerStyle}>
-        <h1 className="xl ls-2">25:00</h1>
+        <h1 className="xl ls-2">{time}</h1>
       </div>
       <p className="md my-3">Let's Get Started...</p>
-      <button className="btn btn-primary md text-white fw-700 my-2 shadow">
+      <button
+        className="btn btn-primary md text-white fw-700 my-2 shadow"
+        onClick={handleTimer}
+      >
         START SESSION
       </button>
     </div>
@@ -18,6 +33,25 @@ const timeContainerStyle = {
   height: "15rem",
   borderRadius: "50%",
   border: "1rem solid #FE9225",
+};
+
+const startTimer = (duration, stateHandler, onFinish) => {
+  let timer = duration;
+  let minutes, seconds;
+  let interval = setInterval(() => {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    timer--;
+
+    if (minutes === "00" && seconds === "00") {
+      clearInterval(interval);
+      onFinish();
+    }
+
+    stateHandler(`${minutes}:${seconds}`);
+  }, 1000);
 };
 
 export default Timer;
